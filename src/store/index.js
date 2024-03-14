e5d7fcc832ff59899418a76edab9393304b8286b
Victor Felisbino Marques <victorfmarques@outlook.com>
2024-03-14 18:19:10 -0300

    feat: creates and provide a store to App.js

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+
+const changeCounterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    return state
+}
+
+const store = createStore(changeCounterReducer);
+
+export default store;
