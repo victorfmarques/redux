9c0afecba10dfd7822ba361f52413ec70c1e8824
Victor Felisbino Marques <victorfmarques@outlook.com>
2024-03-14 18:24:29 -0300

    feat: consumes counter state on App.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,21 @@
+// import for class based components
+// import {connect} from 'react-redux';
+
+// import for function components
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  // consuming store state provided by Provider component set in App.js
+  const counter = useSelector(state => state.counter);
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}> {counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
